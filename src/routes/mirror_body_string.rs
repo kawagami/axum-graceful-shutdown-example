c6b934a0e03e9774167f507a2ea89fee1d98e7ee
kawagami <joelai1988@gmail.com>
2023-12-10 02:33:48 +0800

    update: Extracting a String

---
+++ src/routes/mirror_body_string.rs
@@ -0,0 +1,3 @@
+pub async fn get_handler(body: String) -> String {
+    body
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,11 @@
 mod index;
+mod mirror_body_string;
 
 use axum::{routing::get, Router};
 
 pub fn create_routes() -> Router {
     // Router::new().merge(api::app())
-    Router::new().route("/", get(index::get_handler).post(index::post_handler))
+    Router::new()
+        .route("/", get(index::get_handler).post(index::post_handler))
+        .route("/mirror_body_string", get(mirror_body_string::get_handler))
 }
